043888eaa135f82a1fb7936c789a763749ad9d9a
Ron Kurr <kurr@kurron.org>
2015-12-05 20:23:01 +0000

    Launch script works

---
+++ intellij.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CMD="docker run \
+       --name intellij \
+       --net "host" \
+       --env DISPLAY=unix$DISPLAY \
+       --user 1000:1000 \
+       --volume /tmp/.X11-unix:/tmp/.X11-unix \
+       --volume $HOME:/home/developer \
+       kurron/docker-intellij:latest"
+
+echo $CMD
+$CMD
