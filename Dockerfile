36405a3e50fed8355c39c02b0cc485d5fa248235
Ron Kurr <kurr@kurron.org>
2017-07-01 09:32:08 -0400

    Upgrade to current IntelliJ IDEA and Docker

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,13 @@ FROM kurron/docker-jetbrains-base:latest
 
 MAINTAINER Ron Kurr <kurr@kurron.org>
 
-LABEL org.kurron.ide.name="Intellij IDEA" org.kurron.ide.version=15.0.4
+ENV IDEA_JDK /usr/lib/jvm/zulu-8-amd64
 
-ADD https://download.jetbrains.com/idea/ideaIU-15.0.4.tar.gz /tmp/ide.tar.gz
+ENTRYPOINT ["/opt/idea-IU-171.4694.23/bin/idea.sh"]
 
-RUN mkdir -p /opt/ide && \
-    tar zxvf /tmp/ide.tar.gz --strip-components=1 -C /opt/ide && \
-    rm /tmp/ide.tar.gz
+ADD https://download.jetbrains.com/idea/ideaIU-2017.1.4.tar.gz /opt
+RUN rm -rf /opt/idea-IU-171.4694.23/jre64
 
-ENV IDEA_JDK=/usr/lib/jvm/oracle-jdk-8
 
-ENTRYPOINT ["/opt/ide/bin/idea.sh"]
+USER developer
+
--- idea.sh
+++ idea.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-CMD="docker run \
-       --rm \
-       --interactive \
-       --tty \
-       --name intellij \
-       --net "host" \
-       --env DISPLAY=unix$DISPLAY \
-       --user=$(id -u $(whoami)):$(id -g $(whoami)) \
-       --volume /tmp/.X11-unix:/tmp/.X11-unix \
-       --volume $HOME:/home/developer \
-       --volume /var/run/docker.sock:/var/run/docker.sock \
-       --volume $(which docker):/bin/docker \
-       --volume /usr/lib/x86_64-linux-gnu/libapparmor.so.1.1.0:/lib/x86_64-linux-gnu/libapparmor.so.1 \
-       kurron/docker-intellij:15.0.4"
+# Launches IntelliJ IDEA inside a Docker container
+
+IMAGE=${1:-kurron/docker-intellij:latest}
+
+# Need to give the container access to your windowing system
+xhost +
+
+CMD="docker run --rm \
+                --interactive \
+                --tty \
+                --name intellij \
+                --net "host" \
+                --env DISPLAY=unix$DISPLAY \
+                --user=$(id -u $(whoami)):$(id -g $(whoami)) \
+                --volume /tmp/.X11-unix:/tmp/.X11-unix \
+                --volume $HOME:/home/developer \
+                --volume /var/run/docker.sock:/var/run/docker.sock \
+                ${IMAGE}"
 
 echo $CMD
 $CMD
